c6aaaa46fed9743b2d96566ad87398f09d46b5ee
llauret <lucas.lauret@edu.univ-fcomte.fr>
2023-05-10 10:23:16 +0200

    file added

---
+++ test/view/TestMasterMindView.java
@@ -0,0 +1,15 @@
+package view;
+
+import model.MasterMindPawnPot;
+import model.MasterMindStageModel;
+import model.Pawn;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class TestMasterMindView {
+    @Test
+    public void testCreateLooks() {
+
+    }
+}
